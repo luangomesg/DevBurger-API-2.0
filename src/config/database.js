196f8cba0443f51c0d29acefd4b27275fd0424cf
Luan <luanitachi@hotmail.com>
2024-10-01 22:13:45 -0300

    test

--- src/config/database.js
+++ src/config/database.js
@@ -1,27 +1,17 @@
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
-    url: 'postgresql://devburger_api_d7u3_user:SdePiZ0LuUa3Q69V0Lyx5L8BFVDnqXgK@dpg-cru95ag8fa8c73cafbk0-a.oregon-postgres.render.com/devburger_api_d7u3',
+    url: 'postgresql://devburger_api_d7u3_user:SdePiZ0LuUa3Q69V0Lyx5L8BFVDnqXgK@dpg-cru95ag8fa8c73cafbk0-a.oregon-postgres.render.com/devburger_api_d7u3', // Use a URL completa
     dialect: 'postgres',
-    port: 5432,
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // se necessário, dependendo da configuração de SSL
+        rejectUnauthorized: false,
       },
     },
   },
   test: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
     url: 'postgresql://devburger_api_d7u3_user:SdePiZ0LuUa3Q69V0Lyx5L8BFVDnqXgK@dpg-cru95ag8fa8c73cafbk0-a.oregon-postgres.render.com/devburger_api_d7u3',
     dialect: 'postgres',
-    port: 5432,
     dialectOptions: {
       ssl: {
         require: true,
@@ -30,13 +20,8 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
     url: 'postgresql://devburger_api_d7u3_user:SdePiZ0LuUa3Q69V0Lyx5L8BFVDnqXgK@dpg-cru95ag8fa8c73cafbk0-a.oregon-postgres.render.com/devburger_api_d7u3',
-    port: 5432,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
