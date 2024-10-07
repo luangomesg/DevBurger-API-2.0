ca575f23f6daab45a3f10a9e7142f0db7aa7c2d5
Luan <luanitachi@hotmail.com>
2024-10-06 21:07:12 -0300

    att

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -10,18 +10,16 @@ export default (request, response, next) => {
 
   const [, token] = authToken.split(' ');
 
-  try {
-    jwt.verify(token, authConfig.secret, function (err, decoded) {
-      if (err) {
-        throw new Error();
-      }
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return response.status(401).json({ error: 'Token is invalid' });
+    }
 
-      request.userId = decoded.id;
-      request.userName = decoded.name;
+    // Decodifica o token e atribui valores à requisição
+    request.userId = decoded.id;
+    request.userName = decoded.name;
 
-      return next();
-    });
-  } catch (err) {
-    return response.status(401).json({ error: 'Token is invalid' });
-  }
+    // Continua para o próximo middleware
+    return next();
+  });
 };
